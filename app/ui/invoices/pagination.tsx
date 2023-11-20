3f8d74e9f97b6167e31567bd2bf307e4989f7478
kimsoyol <naywinlhaing@gmail.com>
2023-11-20 12:24:09 +0630

    add pagination

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -4,17 +4,31 @@ import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 import Link from 'next/link';
 import { generatePagination } from '@/app/lib/utils';
+import { usePathname, useSearchParams } from 'next/navigation';
 
 export default function Pagination({ totalPages }: { totalPages: number }) {
   // NOTE: comment in this code when you get to this point in the course
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const currentPage = Number(searchParams.get('page')) || 1;
+  const allPages = generatePagination(currentPage, totalPages);
+ 
+  const createPageURL = (pageNumber: number | string) => {
+    // creates an instance of the current search parameters.
+    const params = new URLSearchParams(searchParams);
 
-  // const allPages = generatePagination(currentPage, totalPages);
+    // updates the "page" parameter to the provided page number.
+    params.set('page', pageNumber.toString());  
+
+    // constructs the full URL using the pathname and updated search parameters.
+    return `${pathname}?${params.toString()}`;
+  };
 
   return (
     <>
       {/* NOTE: comment in this code when you get to this point in the course */}
 
-      {/* <div className="inline-flex">
+      <div className="inline-flex">
         <PaginationArrow
           direction="left"
           href={createPageURL(currentPage - 1)}
@@ -47,7 +61,7 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
           href={createPageURL(currentPage + 1)}
           isDisabled={currentPage >= totalPages}
         />
-      </div> */}
+      </div>
     </>
   );
 }
